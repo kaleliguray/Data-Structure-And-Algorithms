7207c19d75deaf35e73a11e88092d227b827de14
“güray <kaleligiray@gmail.com>
2024-03-29 16:02:56 +0300

    Printing values in the linked List

--- src/linked_list/doubly/DoublyLinkedList.java
+++ src/linked_list/doubly/DoublyLinkedList.java
@@ -1,13 +1,24 @@
 package src.linked_list.doubly;
 
-public class DoublyLinkedList extends ListNode{
+public class DoublyLinkedList {
 
     private ListNode head;
     private ListNode tail;
     private int length;
 
-    public DoublyLinkedList(int data) {
-        super(data);
+
+
+    private class ListNode {
+
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+    public DoublyLinkedList() {
         this.head = null;
         this.tail = null;
         this.length = 0;
@@ -21,7 +32,64 @@ public class DoublyLinkedList extends ListNode{
     public int length() {
         return length;
     }
-    
+
+    /**
+     * method for displaying elements in a forward and a previous way
+     */
+    public void display_forward(){
+
+        if (head == null){
+            System.out.println("List is empty!");
+            return;
+        }
+
+        ListNode temp = head;
+
+        while (temp != null){
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void display_previous(){
+
+        if (tail == null){
+            System.out.println("List is empty");
+            return;
+        }
+
+        ListNode temp = tail;
+
+        while (temp != null){
+            System.out.print(temp.data + " --> ");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+        }
+
+    /**
+     * Insert method adds a value to the List
+      */
+    public void insert_end(int value){
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()){
+            head = newNode;
+        } else {
+            tail.next = newNode ;
+        }
+
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+
+
+
+
+
+
 
 
 
---
+++ src/linked_list/doubly/Main.java
@@ -0,0 +1,23 @@
+package src.linked_list.doubly;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Printing values in the linked List
+
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insert_end(1);
+        dll.insert_end(10);
+        dll.insert_end(15);
+        dll.insert_end(25);
+
+        dll.display_forward();
+
+        dll.display_previous();
+
+    }
+
+
+
+}
