f2cb2da8e19e35afa4d98a661e04d9d46cea2c86
“güray <kaleligiray@gmail.com>
2024-03-31 15:10:20 +0300

    represent binary numbers by using queue

---
+++ src/queue/BinaryNumbers.java
@@ -0,0 +1,46 @@
+package src.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryNumbers {
+
+    public static void main(String[] args) {
+
+        int size = 4;
+
+        String[] result = new String[4];
+
+        Queue<String> que = new LinkedList<>();
+
+        que.offer("1"); // firstly, we should start the queue from 1, and then we will add others to produce binary
+
+        for (int i=0; i<size; i++){
+
+            result[i] = que.poll();
+            String n1 = result[i] + "0";
+            String n2 = result[i] + "1";
+
+            // with offer() method, we add the string n1 & n2 to the queue
+            que.offer(n1);
+            que.offer(n2);
+
+        }
+
+        for (String str: result) {
+            System.out.print(str + " ---> ");
+
+        }
+        
+        String binary = "";
+        for (String str: result) {
+
+            binary = binary + str;
+        }
+
+        System.out.println("\n**** Binary Representation **** ");
+        System.out.println(binary);
+
+    }
+
+}
