1fc766377dfcc154ad756bef1b134b424e1ca3da
“güray <kaleligiray@gmail.com>
2024-04-02 16:07:59 +0300

    spiral matrix

--- src/matrix/SpiralMatrix.java
+++ src/matrix/SpiralMatrix.java
@@ -31,7 +31,7 @@ public class SpiralMatrix {
                 System.out.print(matrix[i][c-1] + " ");
             }
             c--; // decrease column size to go to right column to left
-
+            
             if(k<r){
 
                 // right ---> left
