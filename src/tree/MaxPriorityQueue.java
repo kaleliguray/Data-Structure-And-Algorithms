cc5cb3bdaeaa0f6a9eac5a26d40102d068f27f27
“güray <kaleligiray@gmail.com>
2024-04-03 18:28:14 +0300

    insert, resize and swim method

--- src/tree/MaxPriorityQueue.java
+++ src/tree/MaxPriorityQueue.java
@@ -25,11 +25,64 @@ public class MaxPriorityQueue {
         return n;
     }
 
+    public void resize(int capacity){
+        Integer[] temp = new Integer[capacity];
+
+        for (int i = 0; i< heap.length; i++){
+            temp[i] = heap[i];
+        }
+        heap = temp;
+    }
+
+    public void insert(int x){
+        if (n == heap.length - 1){
+            resize(heap.length*2);
+        }
+        n++;
+        heap[n] = x;
+        swim(n);
+    }
+
+    public void swim(int k){
+
+        while ( k > 1 && heap[k/2] < heap[k]){
+
+            int temp = heap[k];
+            heap[k] = heap[k/2];
+            heap[k/2] = temp;
+            k = k/2; // because we need to shift up till new value inserted is at a correct postion
+        }
+
+    }
+
+    public void print_max_heap(){
+        // we start to count i from 1 because zero index is empty
+        for (int i=1; i<=n; i++){
+            System.out.print(heap[i] + " -> ");
+        }
+    }
+
+
+
+
+
+
+
     public static void main(String[] args) {
 
         MaxPriorityQueue mqp = new MaxPriorityQueue(3);
         System.out.println(mqp.isEmpty());
         System.out.println(mqp.size());
+
+        mqp.insert(4);
+        mqp.insert(5);
+        mqp.insert(2);
+        mqp.insert(6);
+        mqp.insert(1);
+        mqp.insert(3);
+
+        System.out.println(mqp.size());
+        mqp.print_max_heap();
 
 
     }
