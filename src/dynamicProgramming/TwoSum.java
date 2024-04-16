736d30947ad43c855eb51416cfa2fca6c1788974
“güray <kaleligiray@gmail.com>
2024-04-16 22:53:20 +0300

    Dynamic Programming - two sum problem

--- src/dynamicProgramming/TwoSum.java
+++ src/dynamicProgramming/TwoSum.java
@@ -1,5 +1,6 @@
 package src.dynamicProgramming;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,11 +30,55 @@ public class TwoSum {
         throw new IllegalArgumentException("Two numbers not found");
     }
 
+    public static int[] two_sum(int[] arr, int target){
+        Arrays.sort(arr);
+        int left = 0;
+        int right = arr.length - 1;
+        int[] result = new int[2];
+
+        while (left < right){
+            int sum = arr[left] + arr[right];
+            if (sum == target){
+                result[0] = arr[left];
+                result[1] = arr[right];
+                return result;
+
+            }else if (sum < target){
+                left++;
+
+            }else {
+                right--;
+            }
+        }
+        return new int[0];
+    }
+
+    public static void printArray(int[] arr){
+        int n = arr.length;
+
+        for (int i = 0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void arrayDemo(){
+        int[] arr = {11, 2, 0, 10, 7, 6};
+        int[] result = two_sum(arr, 9);
+        printArray(result);
+    }
+
     public static void main(String[] args) {
 
         int[] numbers = new int[] {2, 11, 5, 10, 7, 8};
         int[] result =  twoSum(numbers, 9);
         System.out.println("The two indices are - " + result[0] + " and " + result[1]);
+
+        System.out.println();
+
+        System.out.println("*** 2. two_sum method ***");
+        arrayDemo();
+
 
     }
 
